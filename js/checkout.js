2342001011486fab930092d01918f314fa8536c4
Boe <boe3am@gmail.com>
2022-04-24 23:29:06 +0200

    new

--- js/checkout.js
+++ js/checkout.js
@@ -13,16 +13,16 @@ const message = document.querySelector("#successMessage");
 function verifyForm(event) {
   event.preventDefault();
 
-  if (valueLength(firstName.value, 0) === true) {
-    firstNameError.style.display = "none";
+  if (valueLength(fullName.value, 0)) {
+    fullNameError.style.display = "none";
   } else {
-    firstNameError.style.display = "block";
+    fullNameError.style.display = "block";
   }
 
-  if (valueLength(surName.value, 15) === true) {
-    surNameError.style.display = "none";
+  if (valueLength(cardNumber.value, 15)) {
+    cardNumberError.style.display = "none";
   } else {
-    surNameError.style.display = "block";
+    cardNumberError.style.display = "block";
   }
 
   if (valueLength(expirationDate.value, 3) === true) {
@@ -40,7 +40,6 @@ function verifyForm(event) {
   if (
     valueLength(fullName.value, 0) &&
     valueLength(cardNumber.value, 15) &&
-    valueLength(subject.value, 3) &&
     valueLength(cvv.value, 2)
   ) {
     message.style.display = "block";
